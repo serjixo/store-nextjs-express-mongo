83f8bf22f0370152457601d9e48a7b032a3f8536
Sergio Jimenez <serjicho@hotmail.com>
2023-05-03 13:21:21 +0200

    removed comments on server

--- server/src/routes/products.routes.js
+++ server/src/routes/products.routes.js
@@ -1,10 +1,8 @@
 import express from 'express'
-// import * as dotenv from 'dotenv'
 import {getProductById, getProductByNameAndSorted, getProducts, getProductsSorted} from "../controllers/productsController.js"
 import {pagination} from "../middlewares/pagination.js";
 import {Product} from "../db/models/product.js";
 
-// dotenv.config()
 
 const router = express.Router()
 
--- server/src/schemas/CartSchema.js
+++ server/src/schemas/CartSchema.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 
 export const cartSchema = mongoose.Schema({
-    // id: {
-    //     type: mongoose.Types.ObjectId,
-    //     default: mongoose.Types.ObjectId,
-    //     required: true,
-    // },
+
     productIds: {
         type: [String],
         required: true,
